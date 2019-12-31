8a7ad47247e776cca981361027cea25edcd1c06c
Ravi Singh <ravigkl@gmail.com>
2020-01-01 00:21:16 +0530

    signup user add and error handling

--- README.md
+++ README.md
@@ -4,3 +4,7 @@
 2. go get github.com/dgrijalva/jwt-go
 3. go get github.com/lib/pq
 4. import "golang.org/x/crypto/bcrypt"
+5. go get -u github.com/davecgh/go-spew/spew for json formatting detailed info
+
+##Setup the connectio with Elephant Postgres as follows:
+https://www.elephantsql.com/docs/go.html
--- main.go
+++ main.go
@@ -5,9 +5,47 @@ import (
 	"net/http"
 	"log"
 	"fmt"
+	"database/sql"
+	"encoding/json"
+	"github.com/lib/pq"
+	"github.com/davecgh/go-spew/spew"
 )
 
+//business models for application
+type User struct {
+	ID		int	`json: "id"`
+	Email	string `json: "email"`
+	Password string	`json: "password"`
+}
+type JWT struct {
+	Token string `json: "token"`
+}
+
+type Error struct {
+	Message string `json: "message"`
+}
+
+var db *sql.DB
 func main()  {
+	//pgUrl, err := pq.ParseURL(os.Getenv("ELEPHANTSQL_URL"))
+	pgUrl, err := pq.ParseURL("postgres://ubdonwkq:4-38EnF3sbuKUbVGlptbCikADaOjlwcp@rajje.db.elephantsql.com:5432/ubdonwkq")
+
+	if err != nil {
+		log.Fatal(err)
+	}
+	//fmt.Println(pgUrl)
+	db, err = sql.Open("postgres", pgUrl)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	//fmt.Println(db)
+	//check that connection is established with Db else error
+	err = db.Ping()
+	if err !=nil {
+		log.Fatal("DB connection failed")
+	}
 
 	router := mux.NewRouter()
 	router.HandleFunc("/signup", signup).Methods("POST")
@@ -20,9 +58,30 @@ func main()  {
 	
 }
 
+func respondWithError(w http.ResponseWriter, status int, error Error){
+	w.WriteHeader(status)
+	json.NewEncoder(w).Encode(error)
+}
+
 func signup(w http.ResponseWriter, r *http.Request){
-	fmt.Println("signup invoked")
-	w.Write([]byte("successfully called signup"))
+	var user User
+	var error Error
+
+	json.NewDecoder(r.Body).Decode(&user)
+
+	if user.Email == ""{
+		error.Message = "User name is missing"
+		respondWithError(w, http.StatusBadRequest, error)
+		return
+	}
+	
+	if user.Password == ""{
+		error.Message = "User Password is missing"
+		respondWithError(w, http.StatusBadRequest, error)
+		return
+	}
+	
+	spew.Dump(user)	
 }
 func login(w http.ResponseWriter, r *http.Request){
 	fmt.Println("login invoked")
