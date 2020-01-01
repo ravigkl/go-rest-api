61ab900de3c03df7a735028b583b2aaaf3a1cabc
Ravi Singh <ravigkl@gmail.com>
2020-01-01 09:57:02 +0530

    database connection and encrypted password

--- README.md
+++ README.md
@@ -3,7 +3,7 @@
 1. go get github.com/gorilla/mux
 2. go get github.com/dgrijalva/jwt-go
 3. go get github.com/lib/pq
-4. import "golang.org/x/crypto/bcrypt"
+4. go get golang.org/x/crypto/bcrypt
 5. go get -u github.com/davecgh/go-spew/spew for json formatting detailed info
 
 ##Setup the connectio with Elephant Postgres as follows:
--- main.go
+++ main.go
@@ -9,6 +9,7 @@ import (
 	"encoding/json"
 	"github.com/lib/pq"
 	"github.com/davecgh/go-spew/spew"
+	"golang.org/x/crypto/bcrypt"
 )
 
 //business models for application
@@ -63,6 +64,10 @@ func respondWithError(w http.ResponseWriter, status int, error Error){
 	json.NewEncoder(w).Encode(error)
 }
 
+func responseJSON(w http.ResponseWriter, data interface{}){
+	json.NewEncoder(w).Encode(data)
+}
+
 func signup(w http.ResponseWriter, r *http.Request){
 	var user User
 	var error Error
@@ -80,7 +85,24 @@ func signup(w http.ResponseWriter, r *http.Request){
 		respondWithError(w, http.StatusBadRequest, error)
 		return
 	}
-	
+
+	hash, err := bcrypt.GenerateFromPassword([]byte (user.Password), 10)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+	user.Password= string(hash)
+	stmt := "insert into users (email, password) values ($1, $2) RETURNING id;"
+	err = db.QueryRow(stmt, user.Email, user.Password).Scan(&user.ID)
+	if err != nil {
+		error.Message = "Server Error"
+		respondWithError(w, http.StatusBadRequest, error)
+		return
+	}
+
+	user.Password = ""
+	w.Header().Set("Content-Type", "application/json")
+	responseJSON(w, user)
 	spew.Dump(user)	
 }
 func login(w http.ResponseWriter, r *http.Request){
